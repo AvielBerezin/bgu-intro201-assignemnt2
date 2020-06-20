26a4d00a705b1a3ca400a7d48211d54d3993d157
AvielBerezin <aviel.berezin@gmail.com>
2020-06-20 17:26:25 +0300

    initial commit

---
+++ Assignment2.java
@@ -0,0 +1,120 @@
+
+/* 
+ I, <name> (<ID>), assert that the work I submitted is entirely my own.
+ I have not received any part from any other student in the class, nor did I give parts of it for use to others.
+ I realize that if my work is found to contain code that is not originally my own, a
+ formal case will be opened against me with the BGU disciplinary committee.
+*/
+
+// Last Update: dd/mm/yy
+
+public class Assignment2 {
+
+	
+	/*--------------------------------------------------------
+	   Part a: instance representation & Solution verification 
+	  -------------------------------------------------------
+	 */
+	
+	// Task 1
+	public static boolean hasFlight(int[][] flights, int i, int j) { 
+		// Add your code here
+		return false; 
+	}
+	
+	// Task 2
+	public static boolean isLegalInstance(int[][] flights) {
+		// Add your code here
+		return false; 
+	}
+	
+	// Task 3
+	public static boolean isSolution(int[][] flights, int[] tour) {
+		// Add your code here
+		return false;
+	}
+	
+	/*------------------------------------------------------
+	  Part b: Express the problem as a CNF formula, solve 
+	  it with a SAT solver, and decode the solution
+	  from the satisfying assignment
+	  -----------------------------------------------------
+	 */
+	
+	// Task 4
+	public static int[][] atLeastOne(int[] vars) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 5
+	public static int[][] atMostOne(int[] vars){
+		// Add your code here
+		return null;
+	}
+
+	// Task 6
+	public static int[][] append(int[][] arr1, int[][] arr2) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 7
+	public static int[][] exactlyOne(int[] vars){
+		// Add your code here
+		return null;
+	}
+	
+	// Task 8
+	public static int[][] diff(int[] I1, int[] I2){
+		// Add your code here
+		return null;
+	}
+
+	// Task 9
+	public static int[][] createVarsMap(int n) {
+		// Add your code here
+		return null;
+	}
+
+	// Task 10
+	public static int[][] declareInts(int[][] map) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 11
+	public static int[][] allDiff(int[][] map) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 12
+	public static int[][] allStepsAreLegal(int[][] flights, int[][] map) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 13
+	public static void encode(int[][] flights, int[][] map) {
+		// Add your code here
+	}
+	 
+	// Task 14
+	public static int[] decode(boolean[] assignment, int[][] map) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 15
+	public static int[] solve(int[][] flights) {
+		// Add your code here
+		return null;
+	}
+	
+	// Task 16
+	public static boolean solve2(int[][] flights, int s, int t) {
+		// Add your code here
+		return false;
+	}
+}
